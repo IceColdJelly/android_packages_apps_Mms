d5c27aab6edf03e1e7512b368fbe555f2a9d047a
Roger Chen <cxr514033970@gmail.com>
2012-10-11 16:03:12 +0800

    No need to reset msg after sent a forward msg
    The forwarded conversation will transfor the content as draft
    to the conversation which has the same recipient and no drafts
    
    Change-Id: Ia8b8cebf3832e6524bda487231d928b4c8caa628
    Signed-off-by: Roger Chen <cxr514033970@gmail.com>

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -2663,7 +2663,12 @@ public class ComposeMessageActivity extends Activity
 
     @Override
     public void onPreMessageSent() {
-        runOnUiThread(mResetMessageRunnable);
+        if (mExitOnSent) {
+            // But bail out if we are supposed to exit after the message is sent.
+            finish();
+        } else {
+            runOnUiThread(mResetMessageRunnable);
+        }
     }
 
     @Override
@@ -3821,10 +3826,6 @@ public class ComposeMessageActivity extends Activity
             addRecipientsListeners();
 
             mScrollOnSend = true;   // in the next onQueryComplete, scroll the list to the end.
-        }
-        // But bail out if we are supposed to exit after the message is sent.
-        if (mExitOnSent) {
-            finish();
         }
     }
 
